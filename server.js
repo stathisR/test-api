012ffebdd1ca244f36956fec9eb9496e3d456525
stathisR <slagnecro@hotmail.com>
2022-03-12 15:58:24 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const ingredients = [
+  {
+    "id": "1",
+    "item": "Bacon"
+  },
+  {
+    "id": "2",
+    "item": "Eggs"
+  },
+  {
+    "id": "3",
+    "item": "Milk"
+  },
+  {
+    "id": "4",
+    "item": "Butter"
+  }
+];
+
+app.get('/ingredients', (req, res) =>{
+  res.send(ingredients);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
